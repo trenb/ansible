7be25053709f8d37f66f2e985e538e52e02502c5
Tren Blackburn <trenb@symmetrics.com>
2013-05-03 14:34:11 -0700

    Final testing and fixes for all the roles.

--- roles/ondemand/files/install_ondemand.sh
+++ roles/ondemand/files/install_ondemand.sh
@@ -4,6 +4,7 @@
 #
 JAVA_HOME=/indicee/java/jdk
 PATH=$PATH:/indicee/java/jdk/bin
+PASSWORD=$(grep "password =" /tmp/installer/ondemand.cnf | sed -e 's/password = //g' | head -n1)
 export JAVA_HOME PATH
 logfile=/tmp/installer/conf/db/patch.log
 
@@ -11,7 +12,7 @@ cd /tmp/installer
 mysql --defaults-file=/tmp/installer/ondemand.cnf -uroot -e "update databasechangelog set md5sum=null;" eclipse_app
 SYNERGY_EAR="synergy.ear" ./phoenix_patch.sh --log-level=DEBUG --mysql-host=127.0.0.1 --jboss-run-profile=default app > $logfile 2>&1
 pushd conf/db
-java -jar ../../lib/liquibase.jar --defaultsFile=../../liquibase.properties --changeLogFile=02_patching_eclipse_app_changelog.xml update >> $logfile 2>&1
+java -jar ../../lib/liquibase.jar --driver=com.mysql.jdbc.Driver --classpath=../../lib/mysql-connector-java.jar --url=jdbc:mysql://127.0.0.1:3306/eclipse_app --username=root --password=$PASSWORD --changeLogFile=02_patching_eclipse_app_changelog.xml update >> $logfile 2>&1
 popd
 # Check to see if database imp_cache exists, and if it does, nuke it
 if [[ -d /indicee/mysql-data/db/imp_cache ]]
--- roles/ondemand/files/install_schema.sh
+++ roles/ondemand/files/install_schema.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
 # Helper script to install the base OnDemand schema
 # Tren Blackburn - Symmetrics - April 30 2013
+#
+# Updated May 3 2013
+# Need to run mysql_secure_installation to ensure things are setup for the root user
 if [[ -f "/tmp/installer/andromeda.sql" && ! -d "/indicee/mysql-data/db/eclipse_app" && ! -d "/indicee/mysql-data/db/quartz" ]]
 then
-  mysql --defaults-file=/tmp/installer/ondemand.cnf -uroot < $/tmp/installer/andromeda.sql
+  PASSWORD=$(grep "password =" /tmp/installer/ondemand.cnf | sed -e 's/password = //g' | head -n1)
+  echo -e "\n\n$PASSWORD\n$PASSWORD\n\n\n\n\n" | mysql_secure_installation
+  mysql --defaults-file=/tmp/installer/ondemand.cnf -uroot < /tmp/installer/andromeda.sql
 fi
